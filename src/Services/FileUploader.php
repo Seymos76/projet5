a4d86a932b017885b444e5271db92f06110a7211
Seymos76 <contact@louisthomas.fr>
2018-08-07 10:57:09 +0200

    uploader service is working with current image verification and replacement

--- src/Services/FileUploader.php
+++ src/Services/FileUploader.php
@@ -9,46 +9,17 @@
 namespace App\Services;
 
 
-use App\Entity\User;
 use Symfony\Component\HttpFoundation\File\UploadedFile;
 
 class FileUploader
 {
-    /*private $user;
-
-    public function __construct(User $user)
-    {
-        $this->user = $user;
-    }*/
-
     public function upload(UploadedFile $file, $targetDirectory): string
     {
-        /*switch ($targetDirectory) {
-            case 'avatar_directory':
-                if ($this->getUser()->getAvatar() !== null) {
-                    unlink($this->getUser()->getAvatar(), $targetDirectory);
-                    $this->getUser()->setAvatar(null);
-                }
-                break;
-            case 'bird_directory':
-                // si bird->getPicture() !== null
-                // unset(link);
-                // bird->setPicture(null);
-                break;
-        }*/
-        $file_name = md5(uniqid()).'/'.$file->guessExtension();
+        $file_name = md5(uniqid()).'.'.$file->guessExtension();
         $file->move(
             $targetDirectory,
             $file_name
         );
         return $file_name;
     }
-
-    /**
-     * @return User
-     */
-    /*public function getUser(): User
-    {
-        return $this->user;
-    }*/
 }
