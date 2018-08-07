d7badea722e426b233ce1dc251ebecd3c228d1d1
Caroline Hugues <contact@hc-projet1.ovh>
2018-08-07 22:56:46 +0200

    Ajout d'une route dans le controller

--- src/Controller/ShowCaptureController.php
+++ src/Controller/ShowCaptureController.php
@@ -33,4 +33,14 @@ class ShowCaptureController extends Controller
         return $this->render('Capture\showCapture.html.twig', array('capture' => $capture, 'id' => $id, 'numberOfCaptureComments' => $numberOfCaptureComments, 'form' => $form->createView())); 
     }
 
+    /**
+     * @Route("/observations/", name="observation")
+     * @return Response
+     */
+    public function showCapturesAction(NAOCaptureManager $nAOCaptureManager)
+    {
+        $captures = $nAOCaptureManager->getPublishedCaptures();
+
+        return $this->render('Capture\showCaptures.html.twig', array('captures' => $captures,));
+    }
 }
