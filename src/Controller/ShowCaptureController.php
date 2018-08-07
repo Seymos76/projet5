52e87a70bccc7c6e3268b68da09a3fd085291256
Caroline Hugues <contact@hc-projet1.ovh>
2018-08-07 22:21:50 +0200

    Ajout du controlleur

---
+++ src/Controller/ShowCaptureController.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Capture;
+use App\Entity\User;
+use App\Entity\Comment;
+use App\Services\NAOManager;
+use App\Services\Capture\NAOCaptureManager;
+use App\Form\CommentType;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class ShowCaptureController extends Controller
+{
+    /**
+     * @Route("/observation/{id}", requirements={"id" = "\d+"}, name="observation")
+     * @return Response
+     */
+    public function showCaptureAction($id)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $capture = $em->getRepository(Capture::class)->findOneById($id);
+
+        $numberOfCaptureComments = $em->getRepository(Comment::class)->countCaptureComments($capture);
+
+        $comment = new Comment();
+        $form = $this->get('form.factory')->create(CommentType::class, $comment);
+
+        return $this->render('Capture\showCapture.html.twig', array('capture' => $capture, 'id' => $id, 'numberOfCaptureComments' => $numberOfCaptureComments, 'form' => $form->createView())); 
+    }
+
+}
