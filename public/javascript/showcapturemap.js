bcfc6d3c3e8244fd06bad500689cc155a41ab92c
Caroline Hugues <contact@hc-projet1.ovh>
2018-08-13 12:10:05 +0200

    Mise à jour de l'affichage de la carte d'une observation

--- public/javascript/showcapturemap.js
+++ public/javascript/showcapturemap.js
@@ -1,21 +1,34 @@
 $( function() {
     
 	var macarte = null;
-	var id = map.dataset.identifiant;
+    
+    if (typeof(map0.dataset.identifiant) !== 'undefined')
+    {
+        var id = map0.dataset.identifiant;
+        var url = 'latloncapture/'+id;
+    }
+    else 
+    {
+        var url = 'lastcaptures';
+    }
 
 	function initMap() {
-		$.getJSON('http://localhost:8000/api/latloncapture/'+id+'/', function( data ) {
-            macarte = L.map('map').setView([ data[0].latitude, data[0].longitude], 7);
-            L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-                attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
-                minZoom: 1,
-                maxZoom: 20
-            }).addTo(macarte);
-            var marker = L.marker([data[0].latitude, data[0].longitude]).addTo(macarte);
+        $.getJSON('http://localhost:8000/api/'+url+'/', function( data ) {
+            for (i = 0; i < data.length; i++)
+            {
+                macarte = L.map('map'+i).setView([ data[i].latitude, data[i].longitude], 7);
+                L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+                    attribution: 'données © <a href="//osm.org/copyright">OpenStreetMap</a>/ODbL - rendu <a href="//openstreetmap.fr">OSM France</a>',
+                    minZoom: 1,
+                    maxZoom: 20
+                }).addTo(macarte);
+                var marker = L.marker([data[i].latitude, data[i].longitude]).addTo(macarte);
+            }
         })
     }
     
-	window.onload = function(){
-		initMap(); 
+	window.onload = function()
+    {   
+        initMap(); 
 	};
 });
