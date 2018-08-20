50307138ff651804bde6ab6192a42173f864c768
Caroline Hugues <contact@hc-projet1.ovh>
2018-08-20 12:09:00 +0200

    Update commentFixtures

--- src/DataFixtures/Fixtures/CommentFixtures.php
+++ src/DataFixtures/Fixtures/CommentFixtures.php
@@ -1,7 +1,7 @@
 <?php
 
 // src/DataFixtures/AppFixtures.php
-namespace App\DataFixtures\Fixtures;
+namespace App\DataFixtures;
 
 use App\Entity\User;
 use App\Entity\Capture;
@@ -23,7 +23,7 @@ class CommentFixtures extends Fixture
     {
         $this->em = $em;
         $this->capture1 = $this->em->getRepository(Capture::class)->findOneById('1');
-        $this->capture2 = $this->em->getRepository(Capture::class)->findOneById('2');
+        $this->capture4 = $this->em->getRepository(Capture::class)->findOneById('4');
         $this->user1 = $this->em->getRepository(User::class)->findOneById('1');
         $this->user2 = $this->em->getRepository(User::class)->findOneById('2');
         $this->user3 = $this->em->getRepository(User::class)->findOneById('3');
@@ -43,14 +43,14 @@ class CommentFixtures extends Fixture
         $comment2->setContent('Je pensais pas trouver cette espèce dans cette région !');
         $comment2->setCreatedAt(new \DateTime('now'));
         $comment2->setPublished(false);
-        $comment1->setCapture($this->capture2);
+        $comment1->setCapture($this->capture1);
 
         $comment3 = new Comment();
         $comment3->setAuthor($this->user3);
         $comment3->setContent('Très professionnel cette observation.');
         $comment3->setCreatedAt(new \DateTime('now'));
         $comment3->setPublished(true);
-        $comment1->setCapture($this->capture3);
+        $comment1->setCapture($this->capture4);
 
         $manager->persist($comment1);
         $manager->persist($comment2);
